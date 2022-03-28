895f185e0c99ad37b9ac563ca287321e05da2067
Ben <46061491+BCarlson1512@users.noreply.github.com>
2022-03-27 22:38:11 -0400

    course graph fix

--- apps/client/utils/createPrerequisiteGraph.ts
+++ apps/client/utils/createPrerequisiteGraph.ts
@@ -9,7 +9,8 @@ const createPrerequisiteGraph = (data: any[], requiredCourses?: any[]) => {
   const nodes = [];
   const programSpecific = [];
   // build the set of required courses
-  requiredCourses.map((course) => programSpecific.push(course.get("nodes(q)")[1].properties.id));
+
+  requiredCourses?.map((course) => programSpecific.push(course.get("nodes(q)")[1].properties.id));
   try {
     for (const records of data) {
       for (const rec of records) {
@@ -31,12 +32,12 @@ const createPrerequisiteGraph = (data: any[], requiredCourses?: any[]) => {
           // Check if the edge already exists or this is our principal node. Add edge otherwise.
           if (idx > 0 && !nodes.find((e) => e?.id === `${pre.identity.low}-${rec.at(idx - 1).identity.low}`)) {
             
-            const style = requiredCourses.length > 0 && programSpecific.includes(pre.properties.id) ? {stroke:"blue"} : {};
+            const style = requiredCourses?.length > 0 && programSpecific.includes(pre.properties.id) ? {stroke:"blue"} : {};
             nodes.push({
               id: `${pre.identity.low}-${rec.at(idx - 1).identity.low}`,
               target: pre.identity.low.toString(),
               source: rec.at(idx - 1).identity.low.toString(),
-              animated: requiredCourses.length > 0 && programSpecific.includes(pre.properties.id),
+              animated: requiredCourses?.length > 0 && programSpecific.includes(pre.properties.id),
               style: style,
             });
           }
