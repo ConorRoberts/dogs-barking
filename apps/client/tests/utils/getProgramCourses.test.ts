3bf8789b161112ed64929fd482e05a8a6dbb5337
Ben <46061491+BCarlson1512@users.noreply.github.com>
2022-03-28 02:14:04 -0400

    Eslint is daddy

--- apps/client/tests/utils/getProgramCourses.test.ts
+++ apps/client/tests/utils/getProgramCourses.test.ts
@@ -3,15 +3,15 @@ import { UOFG_COURSE, UOFT_COURSE } from "@config/regex";
 import Course from "@dogs-barking/common/types/Course";
 
 test("Get a program's course based on node id 1x UofG", async () => {
-    const nodeId = "7117";
-    const courses = await getProgramCourses(nodeId);
-    expect(courses.major.every((course: Course) => UOFG_COURSE.test(course.id))).toBeTruthy();
+  const nodeId = "7117";
+  const courses = await getProgramCourses(nodeId);
+  expect(courses.major.every((course: Course) => UOFG_COURSE.test(course.id))).toBeTruthy();
 });
 
 test("Get a program's course based on node id 1x UofT", async () => {
-    const nodeId = "7475";
-    const courses = await getProgramCourses(nodeId);
-    expect(courses.major.every((course: Course) => UOFT_COURSE.test(course.id))).toBeTruthy();
+  const nodeId = "7475";
+  const courses = await getProgramCourses(nodeId);
+  expect(courses.major.every((course: Course) => UOFT_COURSE.test(course.id))).toBeTruthy();
 });
 
 export {};
--- apps/client/tests/utils/getProgramSchool.test.ts
+++ apps/client/tests/utils/getProgramSchool.test.ts
@@ -2,15 +2,15 @@ import getProgramSchool from "@utils/getProgramSchool";
 import schoolSchema from "@schema/schoolSchema";
 
 test("Get a program's school based on node id 1x UofG", async () => {
-    const nodeId = "7117";
-    const school = await getProgramSchool(nodeId);
-    expect(schoolSchema.validate(school)).toBeTruthy();
+  const nodeId = "7117";
+  const school = await getProgramSchool(nodeId);
+  expect(schoolSchema.validate(school)).toBeTruthy();
 });
 
 test("Get a program's school based on node id 1x UofT", async () => {
-    const nodeId = "7475";
-    const school = await getProgramSchool(nodeId);
-    expect(schoolSchema.validate(school)).toBeTruthy();
+  const nodeId = "7475";
+  const school = await getProgramSchool(nodeId);
+  expect(schoolSchema.validate(school)).toBeTruthy();
 });
 
 export {};
