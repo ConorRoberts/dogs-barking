7b16d9d20f50d5e78a9a98d1d6e8f6112fd286ae
Conor Roberts <conor@conorroberts.com>
2022-04-25 00:58:23 -0400

    Removed credentials from amplify config

--- README.md
+++ README.md
@@ -2,13 +2,22 @@
 
 This project serves as a course management utility for University of Guelph Students. It includes a full catalog of all courses and programs, with their respective prerequisite trees available on tap to anyone. It also includes a degree planner feature for students who want to map out their degree programs, semester by semester.
 
-Our project leverages Next.js, hosted on an ECS cluster, for both its frontend and a API. React flow for graphing and Neo4j for our database storage. Our project uses Jest and Cypress as forms of web scraping and unit testing to ensure a clean production build.
-
 ## Getting started
 
-1. Install `pnpm` if not already installed
+1. Install `pnpm`: `npm i -g pnpm`
 2. Run `pnpm install` to install all dependencies
+3. Run `pnpm client` to start up a development server for the frontend
 
-```text
-pnpm install
-```
+## Tech Stack
+
+### Frontend
+
+    - Next.js (Frontend)
+    - AWS ECS (Hosting)
+    - Cypress (Testing)
+
+### Backend
+
+    - Neo4j (Database)
+    - EC2 (Hosting)
+    - Next.js (API)
--- apps/client/config/amplify.ts
+++ apps/client/config/amplify.ts
@@ -1,59 +1,9 @@
 const config = {
   Auth: {
-    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-    // identityPoolId: "us-east-1_R2OZq0wrB",
-
-    // REQUIRED - Amazon Cognito Region
     region: "us-east-1",
-
-    // OPTIONAL - Amazon Cognito Federated Identity Pool Region
-    // Required only if it's different from Amazon Cognito Region
-    // identityPoolRegion: "XX-XXXX-X",
-
-    // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: "us-east-1_R2OZq0wrB",
-
-    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: "71uq7hdjavevjqdup36b9imvdq",
-
-    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-    // mandatorySignIn: false,
-
-    // OPTIONAL - Configuration for cookie storage
-    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
-    // cookieStorage: {
-    // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-    // domain: ".yourdomain.com",
-    // OPTIONAL - Cookie path
-    // path: "/",
-    // OPTIONAL - Cookie expiration in days
-    // expires: 365,
-    // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
-    // sameSite: "lax",
-    // OPTIONAL - Cookie secure flag
-    // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-    // secure: true,
-    // },
-
-    // OPTIONAL - customized storage object
-    // storage: MyStorage,
-
-    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-    // authenticationFlowType: "USER_PASSWORD_AUTH",
-
-    // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
-    // clientMetadata: { myCustomKey: "myCustomValue" },
-
-    // OPTIONAL - Hosted UI configuration
-    // oauth: {
-    //     domain: "your_cognito_domain",
-    //     scope: ["phone", "email", "profile", "openid", "aws.cognito.signin.user.admin"],
-    //     redirectSignIn: "http://localhost:3000/",
-    //     redirectSignOut: "http://localhost:3000/",
-    //     responseType: "code", // or 'token', note that REFRESH token will only be generated when the responseType is code
-    // },
+    userPoolId: process.env.AWS_COGNITO_POOL_ID,
+    userPoolWebClientId: process.env.AWS_COGNITO_WEB_CLIENT_ID,
   },
 };
 
 export default config;
-
