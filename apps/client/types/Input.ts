5adc0ecc8de26810e96538c9e74a961e75c74d76
Conor Roberts <conor@conorroberts.com>
2022-04-30 19:11:57 -0400

    Fixed build error

--- apps/client/types/Input.ts
+++ apps/client/types/Input.ts
@@ -1,54 +1,51 @@
-import { SemestersOffered } from "./Course";
-import School from "./School";
-
 type Input = {
-    Command: string;
-    QueryTypes: Query;
-    Graph: GraphOptions;
-    help: boolean;
-    school: string;
+  Command: string;
+  QueryTypes: Query;
+  Graph: GraphOptions;
+  help: boolean;
+  school: string;
 };
 
 export type Query = {
-    degree: string;
-    major: string;
-    department: string;
-    coursecode: string;
-    school: string;
-    weight: number;
-    coursenum: number;
-    level: number;
-    prerequisite: string[];
-    semester: SemestersOffered[];
-    title: string[];
-    path: boolean;
-    options: AuxOptions;
+  degree: string;
+  major: string;
+  department: string;
+  coursecode: string;
+  school: string;
+  weight: number;
+  coursenum: number;
+  level: number;
+  prerequisite: string[];
+  semester: ("S" | "W" | "F")[];
+  title: string[];
+  path: boolean;
+  options: AuxOptions;
 };
 
 export type GraphOptions = {
-    graphType: "Regular" | "Program";
-    terminal: boolean;
-    graphMajor: string[];
-    includeExternalCourses: boolean;
-    saveAs: string;
-    includeGraduateCourses: boolean;
-    courseLimit: number;
-    hideCoreqs: boolean;
-    showRestrictions: boolean;
-    displayMajor: boolean;
-    displayMinor: boolean;
-    displayArea: boolean;
-    displayOptions: boolean;
-    mergePdf: boolean;
-    semester: string;
-    school: string,
+  graphType: "Regular" | "Program";
+  terminal: boolean;
+  graphMajor: string[];
+  includeExternalCourses: boolean;
+  saveAs: string;
+  includeGraduateCourses: boolean;
+  courseLimit: number;
+  hideCoreqs: boolean;
+  showRestrictions: boolean;
+  displayMajor: boolean;
+  displayMinor: boolean;
+  displayArea: boolean;
+  displayOptions: boolean;
+  mergePdf: boolean;
+  semester: string;
+  school: string;
 };
 
 export type AuxOptions = {
-    SortMode: SortMode;
-    SortDirection: SortDir;
-    Scope: CourseScope;
-    PrintMode: PrintMode;
+  SortMode: SortMode;
+  SortDirection: SortDir;
+  Scope: CourseScope;
+  PrintMode: PrintMode;
 };
 
 export type SortMode = "Raw" | "Weight";
