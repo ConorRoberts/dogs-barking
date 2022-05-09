da7c860cfed102dbbca8e8b69969034286fd8299
Conor Roberts <Conor@ConorRoberts.com>
2022-05-08 23:34:50 -0400

    Updates

--- src/api/createUser/index.js
+++ src/api/createUser/index.js
@@ -10,21 +10,39 @@ exports.handler = async (
 ) => {
     console.log(event);
 
-    const body = JSON.parse(event.body ?? "{}");
-    const query = event.queryStringParameters;
-    const pathParams = event.pathParameters;
+    // const body = JSON.parse(event.body ?? "{}");
+    // const query = event.queryStringParameters;
+    // const pathParams = event.pathParameters;
     const headers = event.headers;
 
-    const { sub } = jwt.decode(headers.authorization.replace("Bearer ", ""));
+    const { sub, birthdate, name, email } = jwt.decode(headers.authorization.replace("Bearer ", ""));
 
     const driver = neo4j.driver(
         `neo4j://${process.env.NEO4J_HOST}`,
         neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
     );
 
+    const session = driver.session();
+
+    const { records } = await session.run(
+        `
+        MERGE (user: User {
+            id: $sub,
+            email: $email,
+            birthdate: date($birthdate),
+            name: $name,
+            school: $school,
+            major: $major,
+            minor: $minor
+        })
+
+        return properties(user) as user
+    `,
+        { sub, email, name, birthdate: birthdate.slice(0, 10), major: "", minor: "", school: "" }
+    );
+
+    await session.close();
     await driver.close();
 
-    return {
-        data: "Hello World",
-    };
+    return records[0].get("user");
 };
