d479d3e5d381bc8470d9cd1c69f398147a1f0697
Conor Roberts <Conor@ConorRoberts.com>
2022-05-09 21:27:27 -0400

    Updates

--- src/client/pages/course/[id].tsx
+++ src/client/pages/course/[id].tsx
@@ -5,17 +5,15 @@ import { Node, Edge } from "react-flow-renderer";
 import createPrerequisiteGraph from "@utils/createPrerequisiteGraph";
 import Rating from "@components/Rating";
 import Link from "next/link";
-import RatingData from "@typedefs/RatingData";
 import MetaData from "@components/MetaData";
 
 interface PageProps {
   course: Course;
   nodes: Node<Course>[];
   edges: Edge[];
-  rating: RatingData;
 }
 
-const Page = ({ course, nodes, edges, rating }: PageProps) => {
+const Page = ({ course, nodes, edges }: PageProps) => {
   return (
     <div className="mx-auto max-w-4xl w-full flex flex-col gap-8 p-2">
       <MetaData title={course.code}>
@@ -37,25 +35,25 @@ const Page = ({ course, nodes, edges, rating }: PageProps) => {
         </p>
       </div>
 
-      {/* <div className="flex flex-row items-center gap-4 justify-center flex-wrap w-full">
+      <div className="flex flex-row items-center gap-4 justify-center flex-wrap w-full">
         <div className="flex flex-col items-center gap-2 flex-1">
           <h3 className="text-center">Difficulty</h3>
-          <Rating courseId={course.id} ratingType="difficulty" initialRating={rating.difficulty} />
+          <Rating courseId={course.id} ratingType="difficulty" initialRating={course.rating.difficulty} />
         </div>
         <div className="flex flex-col items-center gap-2 flex-1">
           <h3 className="text-center">Usefulness</h3>
-          <Rating courseId={course.id} ratingType="usefulness" initialRating={rating.usefulness} />
+          <Rating courseId={course.id} ratingType="usefulness" initialRating={course.rating.usefulness} />
         </div>
         <div className="flex flex-col items-center gap-2 flex-1">
           <h3 className="text-center">Time Spent</h3>
-          <Rating courseId={course.id} ratingType="timeSpent" initialRating={rating.timeSpent} />
+          <Rating courseId={course.id} ratingType="timeSpent" initialRating={course.rating.timeSpent} />
         </div>
-      </div> */}
-      {/* {rating.ratingCount !== undefined && (
+      </div>
+      {course.rating.count !== undefined && (
         <p className="text-gray-500 text-center">
-          This course has been rated {rating.ratingCount} time{rating.ratingCount > 1 && "s"}
+          This course has been rated {course.rating.count} time{course.rating.count > 1 && "s"}
         </p>
-      )} */}
+      )}
       <div>
         <h3 className="text-center mb-1">Prerequisites</h3>
         <CourseGraph nodes={nodes} edges={edges} />
@@ -66,7 +64,8 @@ const Page = ({ course, nodes, edges, rating }: PageProps) => {
 
 export const getServerSideProps = async (context: NextPageContext) => {
   const id = context.query.id as string;
-  const course = await (await fetch(`https://api.dogs-barking.ca/course/${id}`, { method: "GET" })).json();
+  const data = await fetch(`https://api.dogs-barking.ca/course/${id}`, { method: "GET" });
+  const course = await data.json();
   const { nodes, edges } = createPrerequisiteGraph(course);
 
   return {
