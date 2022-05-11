34816ddc22430ab0db32ea522c5c9b1ed8f42836
Conor Roberts <Conor@ConorRoberts.com>
2022-05-11 19:46:58 -0400

    Update index.js

--- src/api/getCourse/index.js
+++ src/api/getCourse/index.js
@@ -1,7 +1,7 @@
 const neo4j = require("neo4j-driver");
 
 /**
-* @method method
+* @method GET
 * @description Gets the course with the given id
 */
 exports.handler = async (
@@ -38,65 +38,65 @@ exports.handler = async (
             avg(rating.usefulness) as usefulness,
             count(rating) as ratingCount
             `,
-        { courseId }
-    );
+    { courseId }
+  );
 
-    await session.close();
-    await driver.close();
+  await session.close();
+  await driver.close();
 
-    // This is to store the requirement tree. Object format is fastest for retrieval and duplicate prevention.
-    const requirements = {};
+  // This is to store the requirement tree. Object format is fastest for retrieval and duplicate prevention.
+  const requirements = {};
 
-    if (records[0].get("requirements") !== null) {
-        records
-            .map((e) => e.get("requirements"))
-            .forEach((list) => {
-                let previous;
-                list.forEach((e, index) => {
-                    const formatted = {
-                        ...e.data,
-                        type: e.type,
-                        requirements: [],
-                    };
+  if (records[0]?.get("requirements") !== null) {
+    records
+      .map((e) => e.get("requirements"))
+      .forEach((list) => {
+        let previous;
+        list.forEach((e, index) => {
+          const formatted = {
+            ...e.data,
+            type: e.type,
+            requirements: [],
+          };
 
-                    // If this is the first element, skip it
-                    if (index !== 0) {
-                        // Are we missing this entry in our list?
-                        if (requirements[formatted.id] === undefined) requirements[formatted.id] = formatted;
+          // If this is the first element, skip it
+          if (index !== 0) {
+            // Are we missing this entry in our list?
+            if (requirements[formatted.id] === undefined) requirements[formatted.id] = formatted;
 
-                        if (index > 1 && previous)
-                            requirements[previous.id].requirements = [
-                                ...new Set([...requirements[previous.id].requirements, formatted.id]),
-                            ];
-                    }
+            if (index > 1 && previous)
+              requirements[previous.id].requirements = [
+                ...new Set([...requirements[previous.id].requirements, formatted.id]),
+              ];
+          }
 
-                    previous = formatted;
-                });
-            });
-    }
+          previous = formatted;
+        });
+      });
+  }
 
-    const fillTree = (id) => {
-        const node = requirements[id];
-
-        return {
-            ...node,
-            requirements: node.requirements.map((e) => fillTree(e)).filter((e) => e !== undefined && e !== null),
-        };
-    };
+  const fillTree = (id) => {
+    const node = requirements[id];
 
     return {
-        ...records[0].get("course"),
-        school: records[0].get("school"),
-        type: "Course",
-        requirements: records
-            .map((e) => fillTree(e.get("requirements")[1].data.id))
-            .map((e, index, arr) => (arr.findIndex((e2) => e2?.id === e?.id) === index ? e : null))
-            .filter((e) => e !== undefined && e !== null),
-        rating: {
-            difficulty: records[0].get("difficulty") ?? 0,
-            usefulness: records[0].get("usefulness") ?? 0,
-            timeSpent: records[0].get("timeSpent") ?? 0,
-            count: records[0].get("ratingCount")?.low ?? 0,
-        }
+      ...node,
+      requirements: node?.requirements?.map((e) => fillTree(e)).filter((e) => e !== undefined && e !== null) ?? [],
     };
+  };
+
+  return {
+    ...records[0].get("course"),
+    school: records[0].get("school"),
+    type: "Course",
+    requirements: records
+      .map((e) => fillTree(e.get("requirements")?.at(1)?.data?.id))
+      .map((e, index, arr) => (arr.findIndex((e2) => e2?.id === e?.id) === index ? e : null))
+      .filter((e) => e !== undefined && e !== null),
+    rating: {
+      difficulty: records[0].get("difficulty") ?? 0,
+      usefulness: records[0].get("usefulness") ?? 0,
+      timeSpent: records[0].get("timeSpent") ?? 0,
+      count: records[0].get("ratingCount")?.low ?? 0,
+    }
+  };
 };
