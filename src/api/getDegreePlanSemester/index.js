c0ecbc1451d732ce8eb95b1827af2bf855893042
Conor Roberts <Conor@ConorRoberts.com>
2022-05-12 18:55:27 -0400

    Updates

--- src/api/getDegreePlanSemester/index.js
+++ src/api/getDegreePlanSemester/index.js
@@ -1,18 +1,39 @@
+const jwt = require("jsonwebtoken");
+const neo4j = require("neo4j-driver");
+
 /**
-* @method method
-* @description description
+* @method GET
+* @description Get a semester with a specific id
 */
 exports.handler = async (
   event
 ) => {
   console.log(event);
 
-  const body = JSON.parse(event.body ?? "{}");
-  const query = event.queryStringParameters;
-  const pathParams = event.pathParameters;
-  const headers = event.headers;
+  // const body = JSON.parse(event.body ?? "{}");
+  // const query = event.queryStringParameters;
+  const { semesterId } = event.pathParameters;
+  const { authorization } = event.headers;
 
-  return {
-    data: "Hello World",
-  };
+  const { sub } = jwt.decode(authorization.replace("Bearer ", ""));
+
+  const driver = neo4j.driver(
+    `neo4j://${process.env.NEO4J_HOST}`,
+    neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
+  );
+
+  const session = driver.session();
+
+  const { records } = await session.run(
+    `
+      MATCH (user:User {id: $userId})-[:HAS]->(dp:DegreePlan)-[:CONTAINS]->(semester: DegreePlanSemester {id: $semesterId})
+      return properties(semester) as semester
+
+      `,
+    { userId: sub, semesterId });
+
+  await session.close();
+  await driver.close();
+
+  return records[0].get("semester");
 };
