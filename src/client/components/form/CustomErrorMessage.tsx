2f4e56039960d456e83b2b150e3a7082abd03809
Conor Roberts <Conor@ConorRoberts.com>
2022-05-16 11:55:35 -0400

    Updated package versions

--- src/client/components/form/CustomErrorMessage.tsx
+++ src/client/components/form/CustomErrorMessage.tsx
@@ -1,11 +1,12 @@
 import { ErrorMessageProps } from "formik";
+import { ReactNode } from "react";
 
 /**
  * Custom error component that is typically placed adjacent to the form field label.
  * @param param0
  * @returns
  */
-const CustomErrorMessage = ({ children, ...props }: ErrorMessageProps) => (
+const CustomErrorMessage = ({ children, ...props }: ErrorMessageProps & { children: ReactNode }) => (
   <p className="text-red-500 text-sm" {...props}>
     {children}
   </p>
