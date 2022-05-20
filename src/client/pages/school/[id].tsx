e1430a0195a5a0f60bf87c548acf38c4a594045a
Conor Roberts <Conor@ConorRoberts.com>
2022-05-19 20:22:11 -0400

    Updates

--- src/client/pages/school/[id].tsx
+++ src/client/pages/school/[id].tsx
@@ -1,14 +1,12 @@
+import { API_URL } from "@config/config";
+import School from "@typedefs/School copy";
 import { NextPageContext } from "next";
 
-/**
- * @description View information about a school
- */
 const Page = ({ school }) => {
   return (
     <div className="mx-auto max-w-3xl">
       <div className="text-center flex flex-col gap-1">
-        {/* <h1>{school.name}</h1> */}
-        <h1>School page</h1>
+        <h1>{school.name}</h1>
         {/* <h5 className="text-gray-600">{school.city}</h5>
         <p className="text-gray-500">{school.abbrev}</p> */}
       </div>
@@ -19,32 +17,16 @@ const Page = ({ school }) => {
   );
 };
 
-// export const getServerSideProps = async (context: NextPageContext) => {
-//   const id = context.query.id as string;
-//   const driver = getNeo4jDriver();
-//   const session = driver.session();
+export const getServerSideProps = async (context: NextPageContext) => {
+  const id = context.query.id as string;
+  const data = await fetch(`${API_URL}/school/${id}`, { method: "GET" });
+  const school = (await data.json()) as School;
 
-//   const data = await session.run(
-//     `
-//     MATCH (school:School)-[:OFFERS]->(course:Course)-[:HAS_RATING]->(rating:Rating)
-//     WHERE id(school) = $id
-//     WITH course,avg(rating.difficulty) as rating, count(rating) as ratingCount
-//     WITH collect({course:course, rating:rating, ratingCount:ratingCount }) as courses, min(rating) as minRating
-//     UNWIND [c in courses where c.rating = minRating] as res
-
-//     return res
-//   `,
-//     { id: +id }
-//   );
-
-//   await session.close();
-//   await driver.close();
-
-//   return {
-//     props: {
-//       school: await getSchool(id),
-//     },
-//   };
-// };
+  return {
+    props: {
+      school,
+    },
+  };
+};
 
 export default Page;
