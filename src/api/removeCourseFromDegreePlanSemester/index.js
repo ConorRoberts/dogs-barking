45f592fc11ae9fcb565990c6d3b828d3e4992617
Conor Roberts <Conor@ConorRoberts.com>
2022-05-22 13:32:43 -0400

    Updates

--- src/api/removeCourseFromDegreePlanSemester/index.js
+++ src/api/removeCourseFromDegreePlanSemester/index.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+const neo4j = require("neo4j-driver");
+
 /**
  * @method DELETE
  * @description Delete a course by ID from a degree plan semester
@@ -5,10 +8,34 @@
 exports.handler = async (event) => {
   console.log(event);
 
-  const body = JSON.parse(event.body ?? "{}");
-  const query = event.queryStringParameters;
-  const pathParams = event.pathParameters;
-  const headers = event.headers;
+  // const body = JSON.parse(event.body ?? "{}");
+  // const query = event.queryStringParameters;
+  const { courseId, semesterId } = event.pathParameters;
+  const { authorization } = event.headers;
 
-  return "Hello World";
+  const { sub } = jwt.decode(authorization.replace("Bearer ", ""));
+
+  const driver = neo4j.driver(
+    `neo4j://${process.env.NEO4J_HOST}`,
+    neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
+  );
+
+  const session = driver.session();
+
+  await session.run(
+    `
+    MATCH 
+      (user: User {id: $userId})-[:HAS]->
+      (plan:DegreePlan)-[:CONTAINS]->
+      (semester:Semester {id: $semesterId})-[r:CONTAINS]->(course:Course {id: $courseId})
+
+      DELETE r
+  `,
+    { userId: sub, courseId, semesterId }
+  );
+
+  await session.close();
+  await driver.close();
+
+  return {};
 };
