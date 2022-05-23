2b86212b176471b8ea93591bd8a425a930c9d710
Conor Roberts <Conor@ConorRoberts.com>
2022-05-23 13:48:05 -0400

    opensearch migrate

--- src/api/searchPrograms/index.js
+++ src/api/searchPrograms/index.js
@@ -1,47 +1,33 @@
-const neo4j = require("neo4j-driver");
+const { Client } = require("@opensearch-project/opensearch");
 
 /**
  * @method GET
- * @description Searches programs
+ * @description Performs an indexed search on all programs
  */
 exports.handler = async (event) => {
   console.log(event);
 
   try {
-    // const body = JSON.parse(event.body ?? "{}");
     const { query } = event.queryStringParameters;
-    // const pathParams = event.pathParameters;
-    // const headers = event.headers;
 
-    const driver = neo4j.driver(
-      `neo4j://${process.env.NEO4J_HOST}`,
-      neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
-    );
+    const client = new Client({
+      node: `https://${process.env.OPENSEARCH_USERNAME}:${process.env.OPENSEARCH_PASSWORD}@${process.env.OPENSEARCH_URL}`,
+    });
 
-    const session = driver.session();
+    const response = await client.search({
+      index: "programs",
+      body: {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ["short", "name"],
+            fuzziness: "AUTO",
+          },
+        },
+      },
+    });
 
-    const { records } = await session.run(
-      `
-              CALL db.index.fulltext.queryNodes("programSearch", $query) 
-              YIELD node, score
-              RETURN properties(node) as program, score
-              order by score DESC 
-              limit(${15})
-            `,
-      {
-        query: `name:${query}* OR short:"${query}"`,
-      }
-    );
-
-    await session.close();
-    await driver.close();
-
-    return records.map((e) => ({
-      id: e.get("program").id,
-      name: e.get("program").name,
-      short: e.get("program").short,
-      degree: e.get("program").degree,
-    }));
+    return response.body.hits.hits.map((hit) => hit._source);
   } catch (error) {
     console.error(error);
     return [];
