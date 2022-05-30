7ef3173c1ca9b5c2b239db24d5826267aaa715cb
Conor Roberts <Conor@ConorRoberts.com>
2022-05-30 00:03:37 -0400

    removed "Inc."

--- src/client/config/config.ts
+++ src/client/config/config.ts
@@ -1,6 +1,6 @@
 // const live = process.env.NODE_ENV === "production";
 
-export const APP_NAME = "Dogs Barking Inc.";
+export const APP_NAME = "Dogs Barking";
 export const API_URL = "https://api.dogs-barking.ca";
 
 export const GOOGLE_MAPS_API_KEY = "AIzaSyBFOXAHpJwLKqnfW8jMjSXWdHXfMkMsvYc";
