f7699e57a799d4c6da920c0730df8a0e48babab0
Conor Roberts <Conor@ConorRoberts.com>
2022-06-07 23:03:41 -0400

    revert

--- src/client/hooks/useSearch.ts
+++ src/client/hooks/useSearch.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { MEILISEARCH_KEY, MEILISEARCH_HOST } from "@config/config";
 import Program from "@typedefs/Program";
 import Course from "@typedefs/Course";
 import MeiliSearch from "meilisearch";
+import axios from "axios";
 
 export interface UseCourseSearchParams {
   courseId: string;
@@ -13,23 +13,17 @@ interface Config {
   type?: "course" | "program";
 }
 
-
 /**
  * Calls the API to get the course search results on update of the query params
  */
 const useSearch = (query: string, config?: Config) => {
   const [results, setResults] = useState<(Course | Program)[]>([]);
   const [loading, setLoading] = useState(false);
-  
+
   const { type = "course" } = config ?? {};
-  
+
   useEffect(() => {
     // Fetch data from search endpoint
-    const client = new MeiliSearch({
-      host: `${location.protocol}//${MEILISEARCH_HOST}`,
-      apiKey: MEILISEARCH_KEY,
-    });
-
     if (query.length === 0) {
       return setResults([]);
     }
@@ -38,11 +32,9 @@ const useSearch = (query: string, config?: Config) => {
       try {
         setLoading(true);
 
-        const index = client.index(`${type}s`);
+        const { data } = await axios.get(`/api/search/${type}`, { params: { query } });
 
-        const { hits } = await index.search(query);
-
-        setResults(hits as (Course | Program)[]);
+        setResults(data as (Course | Program)[]);
       } catch (error) {
         console.error(error);
       } finally {
