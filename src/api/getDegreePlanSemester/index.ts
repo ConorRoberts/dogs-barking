8e7bbc55a848f9c48f19db99c6af3c9443467815
Conor Roberts <Conor@ConorRoberts.com>
2022-06-13 22:00:26 -0400

    updates

--- src/api/getDegreePlanSemester/index.ts
+++ src/api/getDegreePlanSemester/index.ts
@@ -1,29 +1,38 @@
-const jwt = require("jsonwebtoken");
-const neo4j = require("neo4j-driver");
+import neo4j from "neo4j-driver";
+import { APIGatewayEvent, APIGatewayProxyEventPathParameters, APIGatewayProxyResultV2 } from "aws-lambda";
+import { PlannerSemesterData } from "@dogs-barking/common/DegreePlan";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface PathParameters extends APIGatewayProxyEventPathParameters {
+  semesterId: string;
+}
 
 /**
  * @method GET
  * @description Get a semester with a specific id
  */
-exports.handler = async (event) => {
-  console.log(event);
-
-  // const body = JSON.parse(event.body ?? "{}");
-  // const query = event.queryStringParameters;
-  const { semesterId } = event.pathParameters;
-  const { authorization } = event.headers;
-
-  const { sub } = jwt.decode(authorization.replace("Bearer ", ""));
-
+export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResultV2<PlannerSemesterData>> => {
   const driver = neo4j.driver(
     `neo4j://${process.env.NEO4J_HOST}`,
-    neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
+    neo4j.auth.basic(process.env.NEO4J_USERNAME as string, process.env.NEO4J_PASSWORD as string)
   );
 
-  const session = driver.session();
+  try {
+    console.log(event);
+    console.log(event);
 
-  const { records } = await session.run(
-    `
+    // const body = JSON.parse(event.body ?? "{}");
+    // const query = event.queryStringParameters;
+    const { semesterId } = event.pathParameters as PathParameters;
+    const { authorization } = event.headers;
+
+    if (!authorization) throw new Error("Unauthorized");
+    const { sub } = jwt.decode(authorization.replace("Bearer ", "")) as JwtPayload;
+
+    const session = driver.session();
+
+    const { records } = await session.run(
+      `
       MATCH (user:User {id: $userId})-->(dp:DegreePlan)-->(semester: DegreePlanSemester {id: $semesterId})
       
       RETURN
@@ -35,14 +44,20 @@ exports.handler = async (event) => {
         ] as courses,
         properties(semester) as semester
       `,
-    { userId: sub, semesterId }
-  );
+      { userId: sub, semesterId }
+    );
 
-  await session.close();
-  await driver.close();
+    await session.close();
+    await driver.close();
 
-  return {
-    ...records[0].get("semester"),
-    courses: records[0].get("courses").map(({ course, section }) => ({ ...course, section })),
-  };
+    return {
+      ...records[0].get("semester"),
+      courses: records[0].get("courses").map(({ course, section }: any) => ({ ...course, section })),
+    };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await driver.close();
+  }
 };
