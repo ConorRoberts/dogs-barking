4945a636283f84127e5b3de335489e2d3e18cd0c
Conor Roberts <Conor@ConorRoberts.com>
2022-06-13 21:08:20 -0400

    Update index.ts

--- src/api/updatePlannerSemester/index.ts
+++ src/api/updatePlannerSemester/index.ts
@@ -1,41 +1,58 @@
-import jwt from "jsonwebtoken";
 import neo4j from "neo4j-driver";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { APIGatewayEvent, APIGatewayProxyEventPathParameters, APIGatewayProxyResultV2 } from "aws-lambda";
+
+interface PathParameters extends APIGatewayProxyEventPathParameters {
+  semesterId: string;
+}
 
 /**
  * @method POST
  * @description description
  */
-export const handler = async (event) => {
-  console.log(event);
-
-  const { data } = JSON.parse(event.body ?? "{}");
-  // const query = event.queryStringParameters;
-  const { semesterId } = event.pathParameters;
-  const { authorization } = event.headers;
-
-  const { sub } = jwt.decode(authorization.replace("Bearer ", ""));
-
+export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResultV2> => {
   const driver = neo4j.driver(
     `neo4j://${process.env.NEO4J_HOST}`,
     neo4j.auth.basic(process.env.NEO4J_USERNAME as string, process.env.NEO4J_PASSWORD as string)
   );
 
-  const session = driver.session();
+  try {
+    console.log(event);
 
-  const { records } = await session.run(
-    `
-          MATCH (user:User {id: $userId})-[:HAS]->(dp:DegreePlan)-[:CONTAINS]->(semester: DegreePlanSemester {id: $semesterId})
+    const { data } = JSON.parse(event.body ?? "{}");
+    const { semesterId } = event.pathParameters as PathParameters;
+    const { authorization } = event.headers;
 
-          SET semester.semester = $data.semester
-          SET semester.year = $data.year
-          
-          return properties(semester) as semester, [(semester)-[:CONTAINS]->(course:Course) | properties(course)] as courses
+    if (!authorization) throw new Error("Unauthorized");
+    const { sub } = jwt.decode(authorization?.replace("Bearer ", "")) as JwtPayload;
+
+    const session = driver.session();
+
+    const { records } = await session.run(
+      `
+        MATCH (user:User {id: $userId})-[:HAS]->(dp:DegreePlan)-[:CONTAINS]->(semester: DegreePlanSemester {id: $semesterId})
+
+        SET semester.semester = $data.semester
+        SET semester.year = $data.year
+        
+        return 
+          properties(semester) as semester,
+          [(semester)-[:CONTAINS]->(course:Course) | properties(course)] as courses
       `,
-    { data, userId: sub, semesterId }
-  );
+      { data, userId: sub, semesterId }
+    );
 
-  await session.close();
-  await driver.close();
+    await session.close();
+    await driver.close();
 
-  return { ...records[0].get("semester"), courses: records[0].get("courses") };
+    return {
+      ...records[0].get("semester"),
+      courses: records[0].get("courses"),
+    };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await driver.close();
+  }
 };
