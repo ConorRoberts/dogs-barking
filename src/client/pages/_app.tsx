1a0d7063e7175eb9a67f8f8f8c6ccafc2e6a59a3
Conor Roberts <Conor@ConorRoberts.com>
2022-07-11 22:11:57 -0400

    put back essential component

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -4,11 +4,13 @@ import config from "@config/amplify";
 import { Amplify } from "aws-amplify";
 import { Provider } from "react-redux";
 import store from "@redux/store";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(config);
 
 const App = ({ Component, pageProps }) => {
   return (
+    <Authenticator.Provider>
       <Provider store={store}>
         <div
           className="bg-gray-100 dark:bg-gray-900 text-black dark:text-white min-h-screen md:pt-24 pb-24 md:pb-0 relative flex flex-col"
@@ -18,6 +20,7 @@ const App = ({ Component, pageProps }) => {
           <Component {...pageProps} />
         </div>
       </Provider>
+    </Authenticator.Provider>
   );
 };
 
