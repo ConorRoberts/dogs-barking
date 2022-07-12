4671bf372de7e15322a8ea70531c0704ae21fbb8
Conor Roberts <Conor@ConorRoberts.com>
2022-07-12 13:57:38 -0400

    Created function

---
+++ src/api/dogsbarking-sendFeedback/index.ts
@@ -0,0 +1,48 @@
+import { APIGatewayEvent, APIGatewayProxyResultV2 } from "aws-lambda";
+import { SecretsManager } from "@aws-sdk/client-secrets-manager";
+import neo4j from "neo4j-driver";
+
+interface Body {
+  title: string;
+  message: string;
+}
+
+/**
+ * @method POST
+ * @description Sends user feedback from the client to Neo4j
+ */
+export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResultV2> => {
+  const secrets = new SecretsManager({});
+
+  // Get Neo4j credentials
+  const { SecretString: neo4jCredentials } = await secrets.getSecretValue({
+    SecretId: "development/dogs-barking/neo4j",
+  });
+  const { host, username, password } = JSON.parse(neo4jCredentials ?? "{}");
+  const driver = neo4j.driver(`neo4j://${host}`, neo4j.auth.basic(username, password));
+
+  try {
+    console.log(event);
+
+    // const headers = event.headers;
+    const { title, message } = JSON.parse(event.body ?? "{}") as Body;
+
+    const session = driver.session();
+
+    await session.run(
+      `
+        CREATE (:Feedback $data)
+      `,
+      { data: { title, message } }
+    );
+
+    await session.close();
+
+    return {};
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await driver.close();
+  }
+};
