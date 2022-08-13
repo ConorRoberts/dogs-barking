80e427773ba6baf1e627a1560cb1c52b397a4dac
Conor Roberts <Conor@ConorRoberts.com>
2022-08-13 16:23:12 -0400

    updates

--- client/components/form/index.ts
+++ client/components/form/index.ts
@@ -9,3 +9,4 @@ export { default as Switch } from "./Switch";
 export { default as Portal } from "./Portal";
 export { default as CustomErrorMessage } from "./CustomErrorMessage";
 export { default as Toast } from "./Toast";
+export { default as Option } from "./Option";
