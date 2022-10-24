2ec2d5a4964b4a64eee5dbcdf80f4ee4654845df
Conor Roberts <Conor@ConorRoberts.com>
2022-10-23 20:53:03 -0400

    remove staging in lambda trigger

--- apps/api/dogsbarking-setupUser/index.ts
+++ apps/api/dogsbarking-setupUser/index.ts
@@ -1,22 +1,14 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
-import neo4j from "neo4j-driver";
 import { APIGatewayEvent, APIGatewayProxyResultV2 } from "aws-lambda";
-import { SecretsManager } from "@aws-sdk/client-secrets-manager";
+import { getNeo4jDriver } from "@dogs-barking/common";
 
 /**
  * @method POST
  * @description Creates metadata for a Cognito user within Neo4j
  */
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResultV2<object>> => {
-  const { stage } = event.requestContext;
-  const secrets = new SecretsManager({});
-
   // Get Neo4j credentials
-  const { SecretString: neo4jCredentials } = await secrets.getSecretValue({
-    SecretId: `${stage}/dogsbarking/neo4j`,
-  });
-  const { host, username, password } = JSON.parse(neo4jCredentials ?? "{}");
-  const driver = neo4j.driver(`neo4j://${host}`, neo4j.auth.basic(username, password));
+  const driver = await getNeo4jDriver("dev");
 
   try {
     console.log(event);
