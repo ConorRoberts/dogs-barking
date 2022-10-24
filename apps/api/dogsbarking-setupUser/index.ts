315e76302264376a61a6d9a83a8067a39e03d84d
Conor Roberts <Conor@ConorRoberts.com>
2022-10-23 21:13:48 -0400

    Update index.ts

--- apps/api/dogsbarking-setupUser/index.ts
+++ apps/api/dogsbarking-setupUser/index.ts
@@ -1,45 +1,46 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { APIGatewayEvent, APIGatewayProxyResultV2 } from "aws-lambda";
+import { PostConfirmationTriggerHandler } from "aws-lambda";
 import { getNeo4jDriver } from "@dogs-barking/common";
 
 /**
  * @method POST
  * @description Creates metadata for a Cognito user within Neo4j
  */
-export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResultV2<object>> => {
-  // Get Neo4j credentials
+export const handler: PostConfirmationTriggerHandler = async (event) => {
+  console.log(JSON.stringify(event));
+
+  const userData = {
+    id: event.request.userAttributes.sub,
+    name: event.request.userAttributes.name,
+    email: event.request.userAttributes.email,
+    roles: ["user"],
+  };
+
   const driver = await getNeo4jDriver("dev");
 
   try {
-    console.log(event);
-
-    const { authorization } = event.headers;
-    if (!authorization) throw new Error("Unauthorized");
-    const { sub, birthdate, name, email } = jwt.decode(authorization?.replace("Bearer ", "")) as JwtPayload;
-
     const session = driver.session();
 
     const { records } = await session.run(
       `
         MERGE (user: User {
-            id: $sub,
+            id: $id,
             email: $email,
-            birthdate: date($birthdate),
             name: $name,
             school: $school,
             major: $major,
-            minor: $minor
+            minor: $minor,
+            roles: $roles
         })
 
         return properties(user) as user
     `,
-      { sub, email, name, birthdate: birthdate.slice(0, 10), major: "", minor: "", school: "" }
+      { ...userData, major: "", minor: "", school: "" }
     );
 
     await session.close();
     await driver.close();
 
-    return records[0].get("user");
+    return records[0].toObject().user;
   } catch (error) {
     console.error(error);
     throw error;
